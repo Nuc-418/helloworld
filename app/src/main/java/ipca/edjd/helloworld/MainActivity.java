791f3faadefc5a92d98a20ec607f53b69c9a1f89
unknown <rruunneeftw@gmail.com>
2018-09-12 18:26:15 +0100

    Initial commit

---
+++ app/src/main/java/ipca/edjd/helloworld/MainActivity.java
@@ -0,0 +1,37 @@
+package ipca.edjd.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        final TextView textView = findViewById(R.id.textViewHello);
+        Button button = findViewById(R.id.buttonTranslate);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                textView.setText("Olá Mundo!"+count);
+            }
+        });
+
+    }
+
+
+
+
+}
